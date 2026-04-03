e31e234bfab0bc4b80bfe11418b2d87242c0726e
Tine <92650477+TineTheUnc@users.noreply.github.com>
2026-04-04 03:25:01 +0700

    Update autoSpread.lua

--- autoSpread.lua
+++ autoSpread.lua
@@ -25,11 +25,14 @@ end
 
 
 local function checkChild(slot, crop)
-    if crop.isCrop and crop.name ~= 'emptyCrop' then
+    if crop.isCrop then
 
         if crop.name == 'air' then
             action.placeCropStick(2)
 
+        elseif crop.name == 'emptyCrop' then
+            action.placeCropStick()
+            
         elseif scanner.isWeed(crop, 'storage') then
             action.deweed()
             action.placeCropStick()
